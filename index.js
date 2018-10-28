7b4500534db532bb5920622a8af6b5d1f077dc32
‘MR’ <madhu.rebbana@gmail.com>
2018-10-27 20:11:52 -0700

    cors

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 require('babel-register');
 // Start up DB Server
 const mongoose = require('mongoose');
-let MONGODB_URI = 'mongodb://sgc2018:sgc@2018@ds143683.mlab.com:43683/sgc2018';
+let MONGODB_URI = 'mongodb://heroku_c4pljh26:j7tgjs13lhjcpf7j3c80f0vlbs@ds143683.mlab.com:43683/heroku_c4pljh26';
 mongoose.connect(MONGODB_URI);
 // This will require our "app.js" file and immediately call its 'start' method, sending the port from our .env
 require('./app.js').start(process.env.PORT);
--- models/litter.js
+++ models/litter.js
@@ -1,7 +1,7 @@
 'use strict';
 import mongoose from 'mongoose';
 require('dotenv').config();
-let MONGODB_URI = 'mongodb://sgc2018:sgc@2018@ds143683.mlab.com:43683/sgc2018';
+let MONGODB_URI = 'mongodb://heroku_c4pljh26:j7tgjs13lhjcpf7j3c80f0vlbs@ds143683.mlab.com:43683/heroku_c4pljh26';
 mongoose.connect(MONGODB_URI);
 const litterSchema = mongoose.Schema({
   siteName: {type: String, required: true},
