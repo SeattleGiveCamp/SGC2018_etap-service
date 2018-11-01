144b77a11090edecb7c3386e2c6ac7a3d525ed33
Khoa Huynh <khoawin92@gmail.com>
2018-10-31 17:59:06 -0700

    added preventativemeasures in litter model

--- models/litter.js
+++ models/litter.js
@@ -4,13 +4,13 @@ const litterSchema = mongoose.Schema({
   siteName: {type: String, required: true},
   summary: {type:Object},
   siteInfo: {type: Object},
-  siteCondition: {type: Object},
+  siteConditions: {type: Object},
   weightAssessment: {type:Object},
   landUse: {type: Object},
   categories: {type:Object},
   habitatInformation: {type: Object},
   generalObservation: {type: String},
-  
+  preventativeMeasures: {type: Object},
 });
 const Litter = mongoose.model('litter', litterSchema);
 export default Litter;
